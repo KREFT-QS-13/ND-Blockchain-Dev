3738bc0c0836ccdd301724fd0a76faf30122639d
Bartosz Kreft <53482715+KREFT-QS-13@users.noreply.github.com>
2021-11-30 12:48:57 +0100

    Cleaning and finishing Porject 2

--- Decentralized Star Notary Service/truffle-config.js
+++ Decentralized Star Notary Service/truffle-config.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "4872f4ee33974fe6a3a957c9459cb441";
+// Here add your Infura ProjectID for Rinkeby network
+const infuraKey = "<Infura ProjectID>";
 //
 // const fs = require('fs');
-const mnemonic = "hobby document shadow erode witness leisure noble accident borrow analyst use issue";
+// Here add your METAMASK SEED (12 word phrase)
+const mnemonic = "<METAMASK SEED>";
+
 module.exports = {
 
   networks: {
@@ -14,18 +16,20 @@ module.exports = {
      },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    rinkeby: { 
-        provider: () =>
-          new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/${infuraKey}"),
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
         network_id: 4,       // rinkeby's id
         gas: 4500000,        // rinkeby has a lower block limit than mainnet
         gasPrice: 10000000000
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    timeout: 100000
   },
 
   // Configure your compilers
