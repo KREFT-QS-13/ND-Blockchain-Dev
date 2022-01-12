20935165d4663db5481a9b7e837cd4aba9c18c75
Bartosz Kreft <53482715+KREFT-QS-13@users.noreply.github.com>
2022-01-12 21:51:29 +0100

    Tests and fight with Zokrates

--- Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
+++ Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
@@ -1,4 +1,5 @@
 var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+const truffleAssert = require('truffle-assertions');
 
 contract('TestERC721Mintable', accounts => {
 
@@ -9,7 +10,6 @@ contract('TestERC721Mintable', accounts => {
         beforeEach(async function () { 
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
-            // TODO: mint multiple tokens
             await this.contract.mint(account_one, 0);
             await this.contract.mint(account_one, 1);
             await this.contract.mint(account_one, 2);
@@ -19,25 +19,24 @@ contract('TestERC721Mintable', accounts => {
 
         it('should return total supply', async function () { 
             const totalSupply = await this.contract.totalSupply();
-            assert.equal(totalSupply == 4, "The total supply is incorrect.");
+            assert.equal(totalSupply, 4, "The total supply is incorrect.");
         })
 
         it('should get token balance', async function () { 
             const balance = await this.contract.balanceOf(account_one);
-            assert.equal(balance == 3, "The balance is incorrect.");
+            assert.equal(balance, 3, "The balance is incorrect.");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
             const tokenURI = await this.contract.tokenURI(3);
-            assert.equal(tokenURI == "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/3");
+            assert.equal(tokenURI, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/3", "Incorect URI token.");
         })
 
         it('should transfer token from one owner to another', async function () { 
             const tansfer = await this.contract._transferFrom(account_one, account_two, 1, {from: account_one});
             const newowner = await this.contract.ownerOf(1);
             assert.equal(newowner, account_two, 'Incorrect new owenr.');
-            // HERE check if event emitted and that correct item was transfered
         })
     });
 
@@ -47,11 +46,17 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () { 
-            
+            try {
+                var status = await this.contract.mint(account_two, 4, {from: account_one});
+            } catch(err) {
+                status = false;
+            }
+            assert.equal(status, false, "Incorrect address, only contract owner can mint.");
         })
 
         it('should return contract owner', async function () { 
-            
+            let owner = await this.contract.getOwnerAddress();
+            assert.equal(owner, account_one, "Incorect owner address.");
         })
 
     });
