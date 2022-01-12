8348969d243570ef11a22dbf7a4772f4b48746c8
Bartosz Kreft <53482715+KREFT-QS-13@users.noreply.github.com>
2022-01-12 11:57:56 +0100

    Test and last contract in ERC721

--- Blockchain-Capstone/eth-contracts/contracts/ERC721Mintable.sol
+++ Blockchain-Capstone/eth-contracts/contracts/ERC721Mintable.sol
@@ -495,7 +495,7 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
         return _tokenURIs[tokenId];
     }
 
-    function SetTokenURI(uint256 tokenId) internal {
+    function setTokenURI(uint256 tokenId) internal {
         require(_exists(tokenId), "The token already exists.");
         string memory tokenURI = strConcat(_baseTokenURI, uint2str(tokenId));
 
@@ -503,15 +503,10 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     }
 }
 
-//  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
-//  1) Pass in appropriate values for the inherited ERC721Metadata contract
-//      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
-//  2) create a public mint() that does the following:
-//      -can only be executed by the contract owner
-//      -takes in a 'to' address, tokenId, and tokenURI as parameters
-//      -returns a true boolean upon completion of the function
-//      -calls the superclass mint and setTokenURI functions
-
-contract AstroHaousing is ERC721Metadata {
-
+contract AstroHousing is ERC721Metadata("Astro Housing Token", "AHT", "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
+    function mint(address to, uint256 tokenId) public onlyOwner(msg.sender) returns(bool) {
+        super._mint(to, tokenId);
+        super.setTokenURI(tokenId);
+        return true;
+    }
 }
--- Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
+++ Blockchain-Capstone/eth-contracts/test/TestERC721Mintable.js
@@ -10,23 +10,34 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            await this.contract.mint(account_one, 0);
+            await this.contract.mint(account_one, 1);
+            await this.contract.mint(account_one, 2);
+            await this.contract.mint(account_two, 3);
+
         })
 
         it('should return total supply', async function () { 
-            
+            const totalSupply = await this.contract.totalSupply();
+            assert.equal(totalSupply == 4, "The total supply is incorrect.");
         })
 
         it('should get token balance', async function () { 
-            
+            const balance = await this.contract.balanceOf(account_one);
+            assert.equal(balance == 3, "The balance is incorrect.");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
-            
+            const tokenURI = await this.contract.tokenURI(3);
+            assert.equal(tokenURI == "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/3");
         })
 
         it('should transfer token from one owner to another', async function () { 
-            
+            const tansfer = await this.contract._transferFrom(account_one, account_two, 1, {from: account_one});
+            const newowner = await this.contract.ownerOf(1);
+            assert.equal(newowner, account_two, 'Incorrect new owenr.');
+            // HERE check if event emitted and that correct item was transfered
         })
     });
 
