adc7202c015ed3e5acd5164f7e7da3bfba4680f8
Bartosz Kreft <53482715+KREFT-QS-13@users.noreply.github.com>
2022-01-24 23:02:05 +0100

    Fixing tests

--- Blockchain-Capstone/eth-contracts/test/TestSolnSquareVerifier.js
+++ Blockchain-Capstone/eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,6 +1,6 @@
 var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 
-const proof91 = require('./proof91.json');
+const proof39 = require('./proof_3_9.json');
 
 contract('TestSolnSquareVerifier', accounts => {
 
@@ -22,9 +22,9 @@ contract('TestSolnSquareVerifier', accounts => {
             let account_two_balance = await this.contract.balanceOf(account_two);
             assert.equal(account_two_balance, 0, "Wrong balance for account_two, should be 0.");
 
-            await this.contract.mintNewNFT(account_two, 91, proof91.proof, proof91.inputs, {from: account_one});
+            await this.contract.mintNewNFT(account_two, 0, proof39.proof, proof39.inputs, {from: account_one});
 
-            let ownerAddress = await this.contract.ownerOf.call(91, {from: account_one});
+            let ownerAddress = await this.contract.ownerOf.call(0, {from: account_one});
             assert.equal(account_two, ownerAddress, "Token not minted or incorect owener.");
         });
     });
--- Blockchain-Capstone/eth-contracts/test/TestSquareVerifier.js
+++ Blockchain-Capstone/eth-contracts/test/TestSquareVerifier.js
@@ -1,6 +1,6 @@
 var Verifier = artifacts.require('Verifier');
 
-const proof91 = require("./proof91.json");
+const proof39 = require("./proof_3_9.json");
 
 contract('Testing verfier', accounts => {
     const account_one = accounts[0];
@@ -11,13 +11,14 @@ contract('Testing verfier', accounts => {
         });
 
         it('sholud test verification with correct proof', async function () {
-            let correct_proof = await this.contract.verifyTx.call(proof91.proof, proof91.inputs, {from:account_one});
+            console.log(proof39.inputs);
+            let correct_proof = await this.contract.verifyTx.call(proof39.proof, proof39.inputs, {from:account_one});
         
             assert.equal(correct_proof, true, "The proof generated with Zokrates is incorrect.");
         });
 
         it('sholud test verification with incorrect proof', async function () {
-            let incorrect_proof = await this.contract.verifyTx.call(proof91.proof, [0x3, 0x4], {from:account_one});
+            let incorrect_proof = await this.contract.verifyTx.call(proof39.proof, [0x3, 0x4], {from:account_one});
             
             assert.equal(incorrect_proof, false, "Error: The proof generated with diffrent inputs generated corret.");
         });
